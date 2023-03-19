2aecbe12649ec2a8907c126bf7290e671bf4c930
T <tabutyn@gmail.com>
2023-03-19 12:24:41 -0230

    update header with vertex structure, root signature, and vertex buffer objects.

--- include/renderer.h
+++ include/renderer.h
@@ -2,7 +2,9 @@
 #include <string>
 #include <d3d12.h>
 #include <dxgi1_6.h>
+#include <DirectXMath.h>
 
+using namespace DirectX;
 using Microsoft::WRL::ComPtr;
 
 class Renderer {
@@ -23,6 +25,14 @@ public:
 private:
 	static const UINT FrameCount = 2;
 
+	struct {
+		XMFLOAT3 position;
+		XMFLOAT3 color;
+	};
+
+	D3D12_VIEWPORT m_viewport;
+	D3D12_RECT m_scissorRect;
+
 	ComPtr<IDXGIFactory7> m_factory;
 	ComPtr<IDXGIAdapter4> m_adapter;
 	ComPtr<IDXGISwapChain3> m_swapChain;
@@ -30,10 +40,15 @@ private:
 	ComPtr<ID3D12Resource2> m_renderTargets[FrameCount];
 	ComPtr<ID3D12CommandAllocator> m_commandAllocator;
 	ComPtr<ID3D12CommandQueue> m_commandQueue;
+	ComPtr<ID3D12RootSignature> m_rootSignature;
 	ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
 	ComPtr<ID3D12PipelineState> m_pipelineState;
 	ComPtr<ID3D12GraphicsCommandList> m_commandList;
 	ComPtr<ID3D12Fence1> m_fence;
+
+	ComPtr<ID3D12Resource2> m_vertexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+
 	UINT64 m_fenceValue;
 	HANDLE m_fenceEvent;
 
